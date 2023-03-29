5a9af856554d180642113309a877724497ebe454
saeie07 <115580828+saeie07@users.noreply.github.com>
2023-03-29 16:20:55 +0200

    change Login L&F

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -5,6 +5,7 @@ import {Button} from 'components/ui/Button';
 import 'styles/views/Login.scss';
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
+import {FormField} from "components/ui/FormField";
 
 /*
 It is possible to add multiple components inside a single file,
@@ -12,30 +13,6 @@ however be sure not to clutter your files with an endless amount!
 As a rule of thumb, use one file per component and only add small,
 specific components that belong to the main one in the same file.
  */
-const FormField = props => {
-    return (
-        <div className="login field">
-            <label className="login label">
-                {props.label}
-            </label>
-            <input
-                className="login input"
-                placeholder={"enter your " + props.label.toLowerCase()}
-                value={props.value}
-                type={props.type}
-                onChange={e => props.onChange(e.target.value)}
-            />
-        </div>
-    );
-};
-
-FormField.propTypes = {
-    label: PropTypes.string,
-    value: PropTypes.string,
-    type: PropTypes.string,
-    onChange: PropTypes.func
-};
-
 
 const Login = props => {
     const history = useHistory();
