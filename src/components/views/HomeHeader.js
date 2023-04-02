365d9b5b54c0004329bd43c90a50f0c91dfda22a
ringdinglinn <lalaponii@gmail.com>
2023-04-02 22:10:53 +0200

    Update HomeHeader.js

--- src/components/views/HomeHeader.js
+++ src/components/views/HomeHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Button} from 'components/ui/Button';
-import {logoutUser} from 'helpers/api';
+import {logoutUser} from 'helpers/restApi';
 import {useHistory} from 'react-router-dom';
 import "styles/views/HomeHeader.scss";
 import {Link} from "react-router-dom";
