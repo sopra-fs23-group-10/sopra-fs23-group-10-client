c7387350362a0675e9358cbc540e44ac8da5a00d
saeie07 <115580828+saeie07@users.noreply.github.com>
2023-04-13 10:40:44 +0200

    start rules

--- src/components/views/Rules.js
+++ src/components/views/Rules.js
@@ -19,13 +19,12 @@ const Rules = props => {
                 </div>
                 <div className="content font" style={{textAlign: "left"}}>
                     <p>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. <br />
-
+                        Game Idea<br />
+                        Brain Busters is a trivia game designed to entertain and educate players. It can be played as a single player but you also have the opportunity to challenge other players. The game offers two different game modes, the classical trivia game or the image game.
+                        In classic trivia mode, questions are presented in text format, and players must select the correct answer from a list of 4 options. In the image mode however, instead of a text question, blurry pictures are gradually revealed, and players must correctly identify the image as quickly as possible. Both modes challenge players on quick decision-making, as there is a timer, which affects the amount of points players receive.
+                        <br />
+                        The overarching goal is to accumulate as many points as possible and to compete for the ultimate title of Brain Busters champion.<br />
+                        <br />
                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
                         labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
                         et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
