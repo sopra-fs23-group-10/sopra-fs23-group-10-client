a48543aba125a443da0372360b29942133df379e
ringdinglinn <lalaponii@gmail.com>
2023-04-17 16:31:16 +0200

    added profile pic

--- src/components/ui/Player.js
+++ src/components/ui/Player.js
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 import "styles/ui/Player.scss";
+import Identicon from 'react-identicons';
 
 const Player = ({user, action}) => (
     <div
         className="player container" onClick={action ? () => action(user.id) : null}>
-        <img className="player profile-picture" src={user.profilePicture}></img>
+        <Identicon className="player profile-picture" string={user.username}/>
         <div className="player username">{user.username}</div>
         <div className="player rank">{user.points ? user.points : 100}</div>
     </div>
--- src/components/views/HomeHeader.js
+++ src/components/views/HomeHeader.js
@@ -8,6 +8,7 @@ import React,{useEffect,useState} from 'react';
 import User from "../../models/User";
 import "styles/views/PopUp.scss";
 import ReceiveInvitation from "components/views/ReceiveInvitation";
+import Identicon from 'react-identicons';
 
 /**
  * This is an example of a Functional and stateless component (View) in React. Functional components are not classes and thus don't handle internal state changes.
@@ -97,11 +98,14 @@ const HomeHeader = props => {
                     RULES
                 </Link>
             </div>
-            <a className="content fontbold profile"
-                onClick = {() => {setDropDown(!showProfile)}}>
-                {username}
+            <div className="profile-container">
+                <a className="content fontbold profile"
+                    onClick = {() => {setDropDown(!showProfile)}}>
+                    <p className="username">{username}</p>
+                    <Identicon className="profile-picture" string={username}/>
+                </a>
                 {dropDown()}
-            </a>
+            </div>
         </header>
     );
 };
