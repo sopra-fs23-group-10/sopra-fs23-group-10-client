68d5c113f35ea1a153c6c60c5ec9017b5640ec3d
saeie07 <115580828+saeie07@users.noreply.github.com>
2023-04-20 15:38:42 +0200

    Update EndGame.js

--- src/components/views/EndGame.js
+++ src/components/views/EndGame.js
@@ -1,16 +1,137 @@
-import React from 'react';
-//import {api, handleError} from 'helpers/api';
-//import {useHistory, useParams} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {useState} from 'react';
+import {inviteUser, answerInvite, fetchUserById, fetchUsersInGame, finishGame} from 'helpers/restApi';
+import {useHistory, useParams, Link, useLocation} from 'react-router-dom';
 import 'styles/views/EndGame.scss';
 import GameHeader from "./GameHeader";
+import HomeHeader from "./HomeHeader";
 import {Button} from "../ui/Button";
 import 'styles/views/PopUp.scss';
-import {useHistory} from "react-router-dom";
+import { PlayerList } from 'components/ui/PlayerList';
+import { Timer } from 'components/ui/Timer';
+import 'styles/ui/Invitation.scss';
+import ReceiveInvitation from './ReceiveInvitation';
+import BaseContainer from "../ui/BaseContainer";
+import Result from "../../models/Result";
 
 
 
 const EndGame = props => {
     const history = useHistory();
+    const [result, setResult] = useState(null);
+    const [usernameInviting, setUsernameInviting] = useState("");
+    const [usernameInvited, setUsernameInvited] = useState("");
+
+    const { gameMode } = useParams();
+    const [users, setUsers] = useState(null);
+    const [rematchSent, setRematchSent] = useState(false);
+    const [time, setTime] = useState(0);
+    const [previousGame, setPreviousGame] = useState(null);
+
+    useEffect(() => {
+
+        async function fetchGameResults(){
+            try{
+                const response = await finishGame(localStorage.getItem("gameId"));
+                const res = new Result(response);
+                setResult(res);
+                await getUser(response.data.invitingPlayerId, setUsernameInviting);
+                await getUser(response.data.invitedPlayerId, setUsernameInvited);
+            } catch(error){
+                alert(error);
+                history.push("/login")
+            }
+        }
+        const getUser = async (id, callback) => {
+            try{
+                const userData = await fetchUserById(users);
+                callback(userData.username);
+            } catch (error){
+                alert(error);
+            }
+        }
+            fetchGameResults();
+    }, []);
+
+
+    const rematch = async (id) => {
+        try {
+            const response = await inviteUser(id, gameMode.toUpperCase(), "DUEL");
+            localStorage.setItem('gameId', response.id);
+            setRematchSent(true);
+        } catch (error) {
+            history.push("/home");
+        }
+    }
+
+    const cancelRematch = async () => {
+        try {
+            const response = await answerInvite (localStorage.getItem('gameId'), false);
+            localStorage.removeItem('gameId');
+            setRematchSent(false);
+        } catch (error) {
+            history.push("/home");
+        }
+    }
+
+    const getTime = (time) => {
+        setTime(time);
+    }
+
+    const answer = (msg) => {
+        console.log("HANDLE ANSWER INVITING");
+        console.log(msg);
+        const accepted = JSON.parse(msg)[localStorage.getItem('gameId')];
+        if (accepted) {
+            history.push({
+                pathname: '/topic-selection',
+                search: '?update=true',  // query string
+                state: {  // location state
+                    turn: false,
+                    nr: 1,
+                    finished: false
+                },
+            });
+        } else {
+            localStorage.removeItem('gameId');
+            setRematchSent(false);
+        }
+    }
+
+    const sentRematch = () => {
+        if (rematchSent) {
+            return (
+                <div className ='invite-sent'>
+                    <div className = "invitation overlay">
+                    </div>
+                    <div className = "invitation base-container">
+                        <p> Rematch has been sent. Waiting for answer...</p>
+                        <div className="button-container">
+                            <Timer timeLimit={90} timeOut={cancelRematch} getTime={getTime}/>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    const RematchButton = () => {
+        return (
+            <>
+                {sentRematch()}
+                <HomeHeader height = "100" />
+                <ReceiveInvitation onAnswer={answer}/>
+                <div className ='challenge pop up grid'>
+                    <Link to="/home" className = 'back'> x Cancel</Link>
+                    <BaseContainer className = "popup container">
+                        <h3 className='popup title'> Challenge Player</h3>
+                        <PlayerList action ={rematch}/>
+                    </BaseContainer>
+                </div>
+            </>
+        )
+    }
+
     return (
         <>
             <GameHeader height="100"/>
@@ -48,16 +169,16 @@ const EndGame = props => {
                             <Button
                                 width="80%"
                                 style={{margin: "auto"}}
-                                //onClick={() => history.push('/home')}
+                                onClick={RematchButton}
                             >
-                                RESET
+                                REMATCH
                             </Button>
                             <Button
                                 width="80%"
                                 style={{margin: "auto"}}
-                                onClick={() => history.push('/login')}
+                                onClick={() => history.push('/home')}
                             >
-                                GO BACK
+                               RETURN HOME
                             </Button>
                         </div>
                     </div>
