daf07d816af8f0193eae84b34d33a6e26b18db9b
sandrin <sandrin@hunkeler.ws>
2023-05-16 10:20:27 +0200

    fixed random user selection

--- src/components/views/ChallengePlayer.js
+++ src/components/views/ChallengePlayer.js
@@ -80,16 +80,33 @@ const ChallengePlayer = props => {
         });
     }
 
-    function cryptoRandom() {
-        const typedArray = new Uint8Array(1);
-        const randomValue = crypto.getRandomValues(typedArray)[0];
-        return randomValue / Math.pow(2, 8);
+    function cryptoRandom(max) {
+        if (max <= 0) {
+            return 0;
+        }
+
+        const range = max + 1;
+        const bytesNeeded = Math.ceil(Math.log2(range) / 8);
+
+        let randomValue;
+        let randomBytes = new Uint8Array(bytesNeeded);
+        do {
+            crypto.getRandomValues(randomBytes);
+            randomValue = 0;
+            for (let i = 0; i < bytesNeeded; i++) {
+                randomValue = (randomValue << 8) + randomBytes[i];
+            }
+        } while (randomValue >= range * Math.floor(256 / range));
+
+        return randomValue % range;
     }
+
+
 
     const challengeRandomUser = () => {
         const id = localStorage.getItem('id');
         const others = users.filter(user => user.id != parseInt(id));
-        const rnd = cryptoRandom();
+        const rnd = cryptoRandom(others.length-1);
         invite(others[rnd].id).catch(error => {
             console.error(error);
         });
